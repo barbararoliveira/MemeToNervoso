9fa4770a1db26c88b4059c779acb9e1ce83af518
Bárbara <babyr.oliveira@gmail.com>
2020-03-13 22:17:43 -0300

    Realizei o exercício proposto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MemeToNervoso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Usuário, o que você pensa sobre mim?");
+            string fala = Console.ReadLine();
+            fala = fala.Remove (10);
+            Console.WriteLine(fala + "... NÃO, PERA. Tô nervoso.");
+
+            
+        }
+    }
+}
